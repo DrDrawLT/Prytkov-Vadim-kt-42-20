cb9756541cfa0298ebb92a448ffe58de79be2296
Prytkov-Vadim-kt-42-20 <V.Prytkov@yandex.ru>
2023-11-14 16:35:58 +0300

    logging&WeatherForecast_test

---
+++ Prytkov_Vadim_kt-42-20_Lab2/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Prytkov_Vadim_kt_42_20_Lab2
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
