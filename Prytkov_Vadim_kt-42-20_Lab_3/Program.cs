9bfd1c6abd01fb0839de92276cc51f9d15bdff36
Prytkov-Vadim-kt-42-20 <V.Prytkov@yandex.ru>
2024-01-19 12:12:08 +0300

    feat: add ExceptionHandlerMiddleware

--- Prytkov_Vadim_kt-42-20_Lab_3/Program.cs
+++ Prytkov_Vadim_kt-42-20_Lab_3/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Prytkov_Vadim_kt_42_20_Lab_3.DB;
+using Prytkov_Vadim_kt_42_20_Lab_3.Middlewares;
 using static Prytkov_Vadim_kt_42_20_Lab_3.ServiceExtensions.ServiceExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 app.UseAuthorization();
 
